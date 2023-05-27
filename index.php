261002208328b8869675a88636a1a5a86fe09f20
Fornis123 <mheikfornis24@gmail.com>
2023-05-27 11:28:23 +0800

    first commit

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-class
+<?php
+
+include 'employee.php';
+
+header ("Content-Type: application/json; charset=utf-8");
+
+if ($_SERVER["REQUEST_METHOD"] != 'POST') {
+    echo json_encode( [
+        'code' => 500,
+        'messages' => 'only post method is allowed',
+    ]
+    );
+    exit();
+
+}
+
